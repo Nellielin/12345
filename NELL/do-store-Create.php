e15fec9ad97f77a3a51c01192b2b6ce8ca23e1b9
Nellielin <108327198+Nellielin@users.noreply.github.com>
2022-07-12 18:16:03 +0800

    Update do-store-Create.php
    YA!!!!!!!!!!!

--- NELL/do-store-Create.php
+++ NELL/do-store-Create.php
@@ -2,39 +2,40 @@
 require("../db-connect.php");
 if (!isset($_POST["name"])) {
     echo "沒有帶資料到本頁";
-    // exit;
 }
 
-$category_id = $_POST["category_id"];
 $name = $_POST["name"];
 $description = $_POST["description"];
 $discount_number = $_POST["discount_number"];
-$amount = $_POST["amount"];
-$discount_code = $_POST["discount_code"];
-// $upper_limit=$_POST["upper_limit"];
-$lower_limit = $_POST["lower_limit"];
 $start_time = $_POST["start_time"];
 $end_time = $_POST["end_time"];
 $now = date('Y-m-d H:i:s');
-$buyer_valid = $_POST["buyer_valid"];
 
-// $today = date('Y-m-d H:i:s');
+$sqlUpdate = "INSERT INTO discount_store (name, description, discount_number, start_time, end_time, create_time, valid) VALUES ('$name', '$description', '$discount_number', '$start_time', '$end_time', '$now', 1)";
 
-// echo "$name, $email, $phone";
+// $conn->query($sql);
 
-$sql = "INSERT INTO discount (category_id, name, description, amount, discount_number, discount_code, lower_limit, start_time, end_time, create_time, valid) VALUES ('$category_id','$name', '$description', '$amount', '$discount_number', '$discount_code', '$lower_limit',  '$start_time', '$end_time', '$now', 1)";
 
-if (strtotime($now) < strtotime($end_time) && strtotime($now) > strtotime($start_time)) {
-    $sql = "UPDATE discount set buyer_valid= 1 WHERE(start_time <= '$now') and (end_time >= '$now')";
-} else {
-    $sql = "UPDATE discount set buyer_valid= 2 WHERE(start_time <= '$now') and (end_time >= '$now')";
-}
+//有效 -> 1 無效 -> 0
+$sql01 = "UPDATE discount_store set buyer_valid= 1 WHERE (start_time <= create_time) and (end_time >= create_time)";
+$sql00 = "UPDATE discount_store set buyer_valid= 0 WHERE (start_time >= create_time) or (end_time <= create_time)";
 
-if ($conn->query($sql) === TRUE) {
+
+
+
+if ($conn->query($sqlUpdate) === TRUE) {
+    if (strtotime($now) < strtotime($end_time) && strtotime($now) > strtotime($start_time)) {
+        $conn->query($sql01);
+        $conn->query($sql00);
+        // echo"111111";
+    } else {
+        // echo"00000";
+    }
     echo "新資料新增完成";
 } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+    echo "Error: " . $sqlUpdate . "<br>" . $conn->error;
 }
+
 
 $conn->close();
 header("location: create-store-discount.php");
